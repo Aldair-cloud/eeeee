c7f2f36bed7af8e0267e768b68c2208b37d01c24
Aldair-cloud <leonelsanchez@upeu.edu.pe>
2020-11-08 17:46:20 -0500

    v2

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
